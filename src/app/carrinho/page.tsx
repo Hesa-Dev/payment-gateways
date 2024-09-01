09cef7ef058929775cf9321eef3f7231181f344a
hsilvaMR <zagaftuga@gmail.com>
2024-09-01 14:09:32 +0200

    update header , pagination Carrinho de compra

--- src/app/carrinho/page.tsx
+++ src/app/carrinho/page.tsx
@@ -46,44 +46,65 @@ export default function Carrinho() {
             </Link>
           </div>
         ) : (
-          <div className="flex flex-row justify-center   items-center  w-1/2">
-            <div className="flex lg:flex-row  sm:flex-col md:flex-col justify-center  
-            rounded-md border-b-2 border-slate-400 w-full p-1  ">
-              <div className="border border-slate-400 rounded-md p-2">
-                <Image
-                  src={"/products/p1.avif"}
-                  width={150}
-                  height={150}
-                  alt="web"
-                  //   priority={true}
-                  layout="responsive"
-                />
-              </div>
-              <div className="flex flex-col items-center justify-center  p-2 gap-2  w-1/4">
-                <p className="font-bold text-2xl text-slate-400">
-                  TechNova X1
-                </p>
-                <p  className="font-bold text-sm text-slate-400">Em Stock</p>
-                <span  className="font-bold text-base p-2 rounded-md text-slate-600 border">12</span>
-                <span
-                  className="border rounded-md p-2
-                 bg-red-400 text-white cursor-pointer"
-                >
-                  <RiDeleteBinFill className="w-7 h-7" />
-                </span>
-              </div>
+          <div className="flex flex-col justify-center  gap-2  items-center  w-1/2">
+            {carrinho &&
+              carrinho.map(( item:any , index:number) => {
+                return (
+                  <div
+                    className="flex lg:flex-row  sm:flex-col md:flex-col justify-center  
+                rounded-md border-b-2 border-slate-400 w-full p-1 gap-2  " key={index}
+                  >
+                    <div className="border border-slate-400 rounded-md p-2">
+                      <Image
+                        src={item.image3}
+                        width={150}
+                        height={150}
+                        alt="web"
+                        //   priority={true}
+                        // layout="responsive"
+                      />
+                    </div>
+                    <div className="flex flex-col items-center justify-center  p-2 gap-2  w-1/4">
+                      <p className="font-bold text-2xl text-slate-400">
+                        {item.nome}
+                      </p>
+                      <p className="font-bold text-sm text-slate-400">
+                        Em Stock
+                      </p>
+                      <span className="font-bold text-base p-2 rounded-md text-slate-600 border">
+                        12
+                      </span>
+                      <span
+                        className="border rounded-md p-2
+                     bg-red-400 text-white cursor-pointer"
+                      >
+                        <RiDeleteBinFill className="w-7 h-7" />
+                      </span>
+                    </div>
 
-              <div className="flex  w-1/2   justify-end p-2 gap-1 ">
-                <div className=" justify-center flex flex-col gap-2 ">
-                  <span className="font-bold text-base text-slate-400">Qtda</span>
-                  <div>
-                    <input type="number" className="inpt-number" id="quantity"  min="1" max="100"/>
+                    <div className="flex  w-1/2   justify-end p-2 gap-1 ">
+                      <div className=" justify-center flex flex-col gap-2 ">
+                        <span className="font-bold text-base text-slate-400">
+                          Qtda
+                        </span>
+                        <div>
+                          <input
+                            type="number"
+                            className="inpt-number"
+                            id="quantity"
+                            min="1"
+                            max="100"
+                          />
+                        </div>
+
+                        <span className="font-bold text-2xl text-slate-600">
+                          € 112.90
+                        </span>
+                      </div>
+                    </div>
                   </div>
-
-                  <span className="font-bold text-2xl text-slate-600">€ 112.90</span>
-                </div>
-              </div>
-            </div>
+                );
+              })}
           </div>
         )}
       </div>
